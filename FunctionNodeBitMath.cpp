b42aa4c6dc11b4de5316e8409ae55cd89c67e84e
vinivinoo <antoni.vince92@gmail.com>
2026-09-01 16:16:14 +0200

    renamed BitMathModule and renamed variables in it

---
+++ FunctionNodeBitMath.cpp
@@ -0,0 +1,50 @@
+class FunctionNodeBitMath : public Module {
+public:
+  override auto GetFetchInfo() : FetchInfo {
+    return {
+      constants : {"operation0"},
+      input_ports : {"A0", "B0", "Shift0"},
+      output_ports : {"out0"},
+    };
+  }
+
+  override auto GenerateTokenString() : AbstractToken[] {
+    string op_c = GetConstant("operation0");
+
+    switch (op_c) {
+    case "NOT":
+      return {WildcardToken("out0"), TextToken(" = ~"), WildcardToken("A0"),
+              TextToken(";")};
+
+    case "AND":
+    case "OR":
+    case "XOR": {
+      string sign;
+      if (op_c == "AND") {
+        sign = "&";
+      } else if (op_c == "OR") {
+        sign = "|";
+      } else {
+        sign = "^";
+      }
+
+      return {WildcardToken("out0"), TextToken(" = "),
+              WildcardToken("A0"),   TextToken(" " + sign + " "),
+              WildcardToken("B0"),   TextToken(";")};
+    }
+
+    case "SHIFT":
+      return {WildcardToken("out0"),   TextToken(" = glsl_shift("),
+              WildcardToken("A0"),     TextToken(", "),
+              WildcardToken("Shift0"), TextToken(");")};
+
+    case "ROTATE":
+      return {WildcardToken("out0"),   TextToken(" = glsl_rotate("),
+              WildcardToken("A0"),     TextToken(", "),
+              WildcardToken("Shift0"), TextToken(");")};
+
+    default:
+      return {TextToken("/* ERROR: Unknown Bitwise Operation */")};
+    }
+  }
+};
