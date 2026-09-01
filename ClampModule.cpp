f0fe873fd2468fcc8e2ae244ecfa8c4dadf671c0
vinivinoo <antoni.vince92@gmail.com>
2026-09-01 15:08:51 +0200

    added ClampModule.cpp

--- ClampModule.cpp
+++ ClampModule.cpp
@@ -1,25 +1,30 @@
-class ShaderNodeMathModule : public Module {
+class ShaderNodeClampModule : public Module {
+public:
   override auto GetFetchInfo() : FetchInfo {
     return {
-      constants : {"operation0"},
-      input_ports : {"in0", "in1", "in2"},
-      output_ports : {"out0"},
+      constants : {"clamp_type"},
+      input_ports : {"value", "min", "max"},
+      output_ports : {"result"},
     };
   }
 
   override auto GenerateTokenString() : AbstractToken[] {
-    string sign;
+    string clamp_type = GetConstant("clamp_type");
 
-    string op_c = GetConstant("operation0");
-    if (op_c == "ADD") {
-      sign = "+";
-    } else if (op_c == "SUB") {
-      sign = "-";
+    // RANGE
+    if (clamp_type == "RANGE") {
+      return {WildcardToken("result"), TextToken(" = clamp("),
+              WildcardToken("value"),  TextToken(", min("),
+              WildcardToken("min"),    TextToken(", "),
+              WildcardToken("max"),    TextToken("), max("),
+              WildcardToken("min"),    TextToken(", "),
+              WildcardToken("max"),    TextToken("));")};
     }
-    [...]
 
-        return {WildcardToken("value2"), TextToken(" = "),
-                WildcardToken("value0"), TextToken(sign),
-                WildcardToken("value1"), TextToken(";")};
+    // MINMAX
+    return {WildcardToken("result"), TextToken(" = clamp("),
+            WildcardToken("value"),  TextToken(", "),
+            WildcardToken("min"),    TextToken(", "),
+            WildcardToken("max"),    TextToken(");")};
   }
-}
+};
